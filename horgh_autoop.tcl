689d2dac0f1b38bd2c05043f606242cb35c14bd4
horgh <will@summercat.com>
2010-06-26 23:17:06 -0700

    - update README for short descriptions of each script - clean up autoop.tcl a bit

--- horgh_autoop.tcl
+++ horgh_autoop.tcl
@@ -6,18 +6,16 @@
 # Created Thu Oct 23 18:32:36 PDT 2008
 # By horgh
 
-setudef flag horgh_autoop
-
-bind join -|- * horgh_autoop::horgh_autoop
-
 namespace eval horgh_autoop {
+	variable output_cmd putserv
+
+	setudef flag horgh_autoop
+	bind join -|- * horgh_autoop::horgh_autoop
 }
 
 proc horgh_autoop::horgh_autoop {nick host hand chan} {
 	if {![channel get $chan horgh_autoop]} { return }
-	if {[string match -nocase "GoodOne*" $nick]} { return }
-	if {[string match -nocase "*.fr" $host]} { return }
-	quote::putnow "MODE $chan +o $nick"
+	$horgh_autoop::output_cmd "MODE $chan +o $nick"
 }
 
 putlog "horgh_autoop.tcl loaded"
