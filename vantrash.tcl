f38a631dd503e0ae8bf57a9ee833bc0cf4bce31a
horgh <will@summercat.com>
2010-11-03 10:42:57 -0700

    (vantrash) cache the date so we don't fetch so much

--- vantrash.tcl
+++ vantrash.tcl
@@ -9,26 +9,35 @@ package require http
 
 namespace eval vantrash {
 	# corresponds to zone name on vantrash.ca
-	set zone "vancouver-north-blue"
-	set url "http://vantrash.ca/zones/${zone}/nextpickup.txt"
+	variable zone "vancouver-north-blue"
+	variable url "http://vantrash.ca/zones/${zone}/nextpickup.txt"
 
 	# where to output
-	set channel #newhell
+	variable channel #newhell
 
 	# min hr day month year
 	bind time - {30 21 * * *} vantrash::check
+
+	variable cached_date []
 }
 
 proc vantrash::check {min hour day month year} {
-	set token [http::geturl $vantrash::url]
-	set data [http::data $token]
-	http::cleanup $token
+	# Only fetch new date if we haven't yet found one, or that one is past
+	if {$vantrash::cached_date == "" || [clock seconds] > $vantrash::cached_date} {
+		set token [http::geturl $vantrash::url]
+		set data [http::data $token]
+		http::cleanup $token
 
-	set next_date [lindex [split $data] 0]
-	set next_date [clock scan $next_date]
-	set next_day [string trim [clock format $next_date -format %e]]
+		set next_date [lindex [split $data] 0]
+		set vantrash::cached_date [clock scan $next_date]
+		putserv "PRIVMSG $vantrash::channel :(vantrash) Got new date"
+	} else {
+		putserv "PRIVMSG $vantrash::channel :(vantrash) Date is cached"
+	}
 
+	set next_day [string trim [clock format $vantrash::cached_date -format %e]]
 	set tomorrow_day [string trim [clock format [clock scan tomorrow] -format %e]]
+
 	if {$next_day == $tomorrow_day} {
 		putserv "PRIVMSG $vantrash::channel :Garbage day tomorrow!"
 	} else {
